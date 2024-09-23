208fba5e52c6604d2ed3786b493db34288d582e4
vyasv2018 <vyasv2018@gmail.com>
2024-09-23 03:29:18 -0500

    asdfaasd

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,14 @@
+
+import { View, Text } from 'react-native'; 
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+const SignUp = () => {
+    return (
+        <SafeAreaView>
+            <Text>Sign Up here</Text>
+        </SafeAreaView>
+    );
+};
+
+export default SignUp;
---
+++ components/CustomButton.tsx
@@ -0,0 +1,29 @@
+import {TouchableOpacity, Text} from 'react-native';
+import { ButtonProps } from '../types/types.d';
+
+const getBgVariantStyle =(variant: ButtonProps["bgVariant"]) => {
+    
+    switch (variant) {
+        case 'secondary':
+            return "bg-gray-500";
+        case 'danger':
+            return "bg-red-500";
+        case 'success':
+            return "bg-green-500";
+        case 'outline':
+            return "bg-transparent border-neutral-300 border-[0.5px]";
+        default:
+            return "bg-[#0286ff]";
+            }
+}
+const CustomButton = ({onPress, title, bgVariant='primary', textVariant='default', IconLeft, IconRight, className,...props}: ButtonProps) => 
+     (
+        <TouchableOpacity onPress={onPress} className={'w-full rounded-full flex flex-row justify-center items-center shadow-md shadow-neutral-400/70 $#{getBgVariantStyle (bgVariant)} ${className}'}> 
+        {IconLeft && <IconLeft />}
+        <Text>{title}</Text>
+        {IconRight && <IconRight />} 
+        </TouchableOpacity>
+    )
+
+
+export default CustomButton;
